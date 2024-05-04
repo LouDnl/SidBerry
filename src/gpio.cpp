df2d6a94b24e1b1e3f38d91ee8b64afe7a30e82b
loudnl <LouDnl@users.noreply.github.com>
2024-05-04 12:36:52 +0200

    Add supporting ftdi gpio source and header file

---
+++ src/gpio.cpp
@@ -0,0 +1,332 @@
+//============================================================================
+// Description : FTDI ft2232h supporting GPIO source file for SidBerry
+// Author      : LouD
+// Year        : 2024
+//============================================================================
+#include "gpio.h"
+
+struct ftdi_context *ftdi1, *ftdi2;
+int f_port, gpiop, gpiopin, gpioport, gpiolevel, gpiostate;
+unsigned char maskport1[1];
+unsigned char maskport2[1];
+unsigned char gpiobuff1[1];
+unsigned char gpiobuff2[1];
+
+/* Helpers */
+int calcPin(uint8_t pin, uint8_t port)
+{
+    return port == PORT1 ? pin - PORTA : port == PORT2 ? pin - PORTB
+                                                       : 0;
+}
+
+int idPortByPin(uint8_t pin)
+{
+    return (pin >= PORTA) && (pin < PORTB) ? PORT1 : (pin >= PORTB) && (pin < 30) ? PORT2
+                                                                                  : 0;
+}
+
+int idPort(uint8_t port)
+{
+    return (port == 1) || (port == 10) ? PORT1 : (port == 2) || (port == 20) ? PORT2
+                                                                             : 0;
+}
+
+int setupBitBang()
+{
+    DBG("\n");
+
+    // Init 1. channel
+    if ((ftdi1 = ftdi_new()) == 0)
+    {
+        fprintf(stderr, "ftdi_new failed\n");
+        return EXIT_FAILURE;
+    }
+
+    ftdi_set_interface(ftdi1, INTERFACE_A);
+    f_port = ftdi_usb_open(ftdi1, VENDOR, PRODUCT);
+    if (f_port < 0 && f_port != -5)
+    {
+        fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f_port, ftdi_get_error_string(ftdi1));
+        ftdi_free(ftdi1);
+        exit(-1);
+    }
+    DBG("ftdi open succeeded(channel 1): %d\n", f_port);
+
+    DBG("enabling bitbang mode(channel 1)\n");
+    ftdi_set_bitmode(ftdi1, 0xFF, BITMODE_BITBANG);
+
+    // Init 2. channel
+    if ((ftdi2 = ftdi_new()) == 0)
+    {
+        fprintf(stderr, "ftdi_new failed\n");
+        return EXIT_FAILURE;
+    }
+    ftdi_set_interface(ftdi2, INTERFACE_B);
+    f_port = ftdi_usb_open(ftdi2, VENDOR, PRODUCT);
+    if (f_port < 0 && f_port != -5)
+    {
+        fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f_port, ftdi_get_error_string(ftdi2));
+        ftdi_free(ftdi2);
+        exit(-1);
+    }
+    DBG("ftdi open succeeded(channel 2): %d\n", f_port);
+
+    DBG("enabling bitbang mode (channel 2)\n");
+    ftdi_set_bitmode(ftdi2, 0xFF, BITMODE_BITBANG);
+
+    // buf[0]* = BITMASK;
+
+    return 0;
+}
+
+int breakdownBitBang()
+{
+    // struct ftdi_context *ftdi1, *ftdi2;
+    DBG("disabling bitbang mode(channel 1)\n");
+    ftdi_disable_bitbang(ftdi1);
+    ftdi_usb_close(ftdi1);
+    ftdi_free(ftdi1);
+
+    DBG("disabling bitbang mode(channel 2)\n");
+
+    ftdi_disable_bitbang(ftdi2);
+    ftdi_usb_close(ftdi2);
+    ftdi_free(ftdi2);
+    return 0;
+}
+
+int initPort(uint8_t port)
+{
+
+    gpioport = idPort(port);
+
+    DBG("\n");
+
+    if (gpioport == 1)
+    { // Init channel
+        if ((ftdi1 = ftdi_new()) == 0)
+        {
+            fprintf(stderr, "ftdi_new failed\n");
+            return EXIT_FAILURE;
+        }
+
+        ftdi_set_interface(ftdi1, PORT1);
+        f_port = ftdi_usb_open(ftdi1, VENDOR, PRODUCT);
+        if (f_port < 0 && f_port != -5)
+        {
+            fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f_port, ftdi_get_error_string(ftdi1));
+            ftdi_free(ftdi1);
+            exit(-1);
+        }
+    }
+    else if (gpioport == 2)
+    { // Init channel
+        if ((ftdi2 = ftdi_new()) == 0)
+        {
+            fprintf(stderr, "ftdi_new failed\n");
+            return EXIT_FAILURE;
+        }
+
+        ftdi_set_interface(ftdi2, PORT2);
+        f_port = ftdi_usb_open(ftdi2, VENDOR, PRODUCT);
+        if (f_port < 0 && f_port != -5)
+        {
+            fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f_port, ftdi_get_error_string(ftdi2));
+            ftdi_free(ftdi2);
+            exit(-1);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "%d is not a valid port!\n", gpioport);
+        return 1;
+    }
+    DBG("ftdi open succeeded(channel %i): %d\n", gpioport, f_port);
+    return 0;
+}
+
+int enableBitBang(uint8_t port)
+{
+    gpioport = idPort(port);
+
+    DBG("enabling bitbang all pins output mode(channel %i)\n", gpioport);
+
+    if (gpioport == 1)
+    {
+        ftdi_set_bitmode(ftdi1, 0xFF, BITMODE_BITBANG);
+    }
+    else if (gpioport == 2)
+    {
+        ftdi_set_bitmode(ftdi2, 0xFF, BITMODE_BITBANG);
+    }
+    else
+    {
+        fprintf(stderr, "%d is not a valid port!\n", gpioport);
+        return 1;
+    }
+    return 0;
+}
+
+int disableBitBang(uint8_t port)
+{
+
+    gpioport = idPort(port);
+
+    DBG("disabling bitbang mode(channel %i)\n", gpioport);
+
+    if (gpioport == 1)
+    {
+        ftdi_disable_bitbang(ftdi1);
+    }
+    else if (gpioport == 2)
+    {
+        ftdi_disable_bitbang(ftdi2);
+    }
+    else
+    {
+        fprintf(stderr, "%d is not a valid port!\n", gpioport);
+        return 1;
+    }
+    return 0;
+}
+
+int closePort(uint8_t port)
+{
+
+    gpioport = idPort(port);
+
+    DBG("closing port %i\n", gpioport);
+
+    if (gpioport == 1)
+    {
+        ftdi_usb_close(ftdi1);
+        ftdi_free(ftdi1);
+    }
+    else if (gpioport == 2)
+    {
+        ftdi_usb_close(ftdi2);
+        ftdi_free(ftdi2);
+    }
+    else
+    {
+        fprintf(stderr, "%d is not a valid port!\n", gpioport);
+        return 1;
+    }
+    return 0;
+}
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+    gpioport = idPortByPin(pin);
+    gpiop = calcPin(pin, gpioport);
+    switch (gpioport)
+    {
+    case 1:
+        if (mode == 0)
+        {
+            maskport1[0] &= ~(1 << gpiop);
+        }
+        else
+        {
+            maskport1[0] |= mode << gpiop;
+        }
+        DBG("set gpioport: %i, pin: %i, mode: %i, mask: " PRINTF_BINARY_PATTERN_INT8 "\n",
+            gpioport, gpiop, mode, PRINTF_BYTE_TO_BINARY_INT8(maskport1[0]));
+        f_port = ftdi_set_bitmode(ftdi1, maskport1[0], BITMODE_BITBANG);
+        break;
+    case 2:
+        if (mode == 0)
+        {
+            maskport2[0] &= ~(1 << gpiop);
+        }
+        else
+        {
+            maskport2[0] |= mode << gpiop;
+        }
+        DBG("set gpioport: %i, pin: %i, mode: %i, mask: " PRINTF_BINARY_PATTERN_INT8 "\n",
+            gpioport, gpiop, mode, PRINTF_BYTE_TO_BINARY_INT8(maskport2[0]));
+        f_port = ftdi_set_bitmode(ftdi2, maskport2[0], BITMODE_BITBANG);
+        break;
+    default:
+        break;
+    }
+}
+
+void digitalWrite(uint8_t pin, int level)
+{
+
+    gpioport = idPortByPin(pin);
+    gpiop = calcPin(pin, gpioport);
+
+    switch (gpioport)
+    {
+    case 1:
+
+        f_port = ftdi_read_pins(ftdi1, gpiobuff1);
+        DBG("gpioport: %i, pin: %i, level: %i, read buff: " PRINTF_BINARY_PATTERN_INT8 " | ",
+            gpioport, gpiop, level, PRINTF_BYTE_TO_BINARY_INT8(gpiobuff1[0]));
+        if (level == 0)
+        {
+            gpiobuff1[0] &= ~(1 << gpiop);
+        }
+        else
+        {
+            gpiobuff1[0] |= level << gpiop;
+        }
+        f_port = ftdi_write_data(ftdi1, gpiobuff1, 1);
+        DBG("write buff: " PRINTF_BINARY_PATTERN_INT8 "\n",
+            PRINTF_BYTE_TO_BINARY_INT8(gpiobuff1[0]));
+        break;
+    case 2:
+
+        f_port = ftdi_read_pins(ftdi2, gpiobuff2);
+        DBG("gpioport: %i, pin: %i, level: %i, read buff: " PRINTF_BINARY_PATTERN_INT8 " | ",
+            gpioport, gpiop, level, PRINTF_BYTE_TO_BINARY_INT8(gpiobuff2[0]));
+        if (level == 0)
+        {
+            gpiobuff2[0] &= ~(1 << gpiop);
+        }
+        else
+        {
+            gpiobuff2[0] |= level << gpiop;
+        }
+        f_port = ftdi_write_data(ftdi2, gpiobuff2, 1);
+        DBG("write buff: " PRINTF_BINARY_PATTERN_INT8 "\n",
+            PRINTF_BYTE_TO_BINARY_INT8(gpiobuff2[0]));
+
+        break;
+    default:
+        break;
+    }
+}
+
+int digitalRead(uint8_t pin)
+{
+
+    gpioport = idPortByPin(pin);
+    gpiop = calcPin(pin, gpioport);
+    int result;
+
+    switch (gpioport)
+    {
+    case 1:
+
+        f_port = ftdi_read_pins(ftdi1, gpiobuff1);
+        DBG("gpioport: %i, pin: %i, read buff: " PRINTF_BINARY_PATTERN_INT8 " | ",
+            gpioport, gpiop, PRINTF_BYTE_TO_BINARY_INT8(gpiobuff1[0]));
+        result = gpiobuff1[0] >> gpiop;
+        break;
+    case 2:
+
+        f_port = ftdi_read_pins(ftdi2, gpiobuff2);
+        DBG("gpioport: %i, pin: %i, read buff: " PRINTF_BINARY_PATTERN_INT8 " | ",
+            gpioport, gpiop, PRINTF_BYTE_TO_BINARY_INT8(gpiobuff2[0]));
+        result = gpiobuff2[0] >> gpiop;
+        break;
+
+    default:
+        result = -1;
+        break;
+    }
+    return result;
+}
---
+++ src/gpio.h
@@ -0,0 +1,96 @@
+//============================================================================
+// Description : FTDI ft2232h supporting GPIO header file for SidBerry
+// Author      : LouD
+// Year        : 2024
+//============================================================================
+
+#pragma once
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ftdi.hpp>
+
+#include "sidberry.h"
+#include "macros.h"
+
+/* Helpers */
+extern int calcPin(uint8_t pin, uint8_t port);
+/* Helpers */
+extern int idPortByPin(uint8_t pin);
+/* Helpers */
+extern int idPort(uint8_t port);
+
+/* BitBang all pins output */
+extern int setupBitBang();
+
+/* BitBang full deinitializer */
+extern int breakdownBitBang();
+
+extern int initPort(uint8_t port);
+extern int enableBitBang(uint8_t port);
+extern int disableBitBang(uint8_t port);
+extern int closePort(uint8_t port);
+extern void pinMode(uint8_t pin, uint8_t mode);
+
+/* Set a single pin to HIGH or LOW in BitBing mode */
+extern void digitalWrite(uint8_t pin, int level);
+/* Read a single pin in BitBang mode */
+extern int digitalRead(uint8_t pin);
+
+extern struct ftdi_context *ftdi1, *ftdi2;
+extern int f_port, gpiop, gpiopin, gpioport, gpiolevel, gpiostate;
+extern unsigned char maskport1[1];
+extern unsigned char maskport2[1];
+extern unsigned char gpiobuff1[1]; // = 0xFF;
+extern unsigned char gpiobuff2[1]; // = 0xFF;
+
+// #define INVERTEDLOGIC
+#ifndef INVERTEDLOGIC
+#define ON 1
+#define OFF 0
+#else
+#define ON 0
+#define OFF 1
+#endif
+#define INPUT 0
+#define OUTPUT 1
+#define HIGH ON
+#define LOW OFF
+
+#define VENDOR 0x0403
+#define PRODUCT 0x6010
+
+#define PORTMASK 0xFF
+#define NULLMASK 0x0
+
+#define PORT1 INTERFACE_A // AD0 ~ AD7
+#define PORT2 INTERFACE_B // BD0 ~ BD7
+
+#define PORTA 10
+#define PORTB 20
+
+/* PORT1 */
+#define AD0 10
+#define AD1 11
+#define AD2 12
+#define AD3 13
+#define AD4 14
+#define AD5 15
+#define AD6 16
+#define AD7 17
+
+/* PORT2 */
+#define BD0 20
+#define BD1 21
+#define BD2 22
+#define BD3 23
+#define BD4 24
+#define BD5 25
+#define BD6 26
+#define BD7 27
+
+#endif
