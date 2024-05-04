e62431638c5ed6b4fd2bf053d6574d5e2da12685
loudnl <LouDnl@users.noreply.github.com>
2024-05-04 12:33:29 +0200

    add supporting sidfile functions

--- src/SidFile.cpp
+++ src/SidFile.cpp
@@ -2,7 +2,14 @@
 
 SidFile::SidFile()
 {
-	
+}
+
+unsigned char SidFile::reverse(unsigned char b)
+{
+	b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+	b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+	b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+	return b;
 }
 
 uint16_t SidFile::Read16(const uint8_t *p, int offset)
@@ -25,32 +32,32 @@ bool SidFile::IsPSIDHeader(const uint8_t *p)
 int SidFile::Parse(string file)
 {
 	FILE *f = fopen(file.c_str(), "rb");
-	
-	if(f == NULL)
+
+	if (f == NULL)
 	{
 		return SIDFILE_ERROR_FILENOTFOUND;
 	}
-	
+
 	uint8_t header[PSID_MAX_HEADER_LENGTH];
 	memset(header, 0, PSID_MAX_HEADER_LENGTH);
-	
+
 	size_t read = fread(header, 1, PSID_MAX_HEADER_LENGTH, f);
-	
-	if(read < PSID_MIN_HEADER_LENGTH || !IsPSIDHeader(header))
+
+	if (read < PSID_MIN_HEADER_LENGTH || !IsPSIDHeader(header))
 	{
 		fclose(f);
 		return SIDFILE_ERROR_MALFORMED;
 	}
 
 	numOfSongs = Read16(header, SIDFILE_PSID_NUMBER);
-	
-	if(numOfSongs == 0)
+
+	if (numOfSongs == 0)
 	{
 		numOfSongs = 1;
 	}
-	
+
 	firstSong = Read16(header, SIDFILE_PSID_DEFSONG);
-	if(firstSong)
+	if (firstSong)
 	{
 		firstSong--;
 	}
@@ -59,39 +66,84 @@ int SidFile::Parse(string file)
 		firstSong = 0;
 	}
 
+	dataOffset = Read16(header, SIDFILE_PSID_LENGTH);
 	initAddr = Read16(header, SIDFILE_PSID_INIT);
 	playAddr = Read16(header, SIDFILE_PSID_MAIN);
 	speedFlags = Read32(header, SIDFILE_PSID_SPEED);
 
 	moduleName = (char *)(header + SIDFILE_PSID_NAME);
-	
+
 	authorName = (char *)(header + SIDFILE_PSID_AUTHOR);
-	
-	string copyrightInfo = (char *)(header + SIDFILE_PSID_COPYRIGHT);
+
+	copyrightInfo = (char *)(header + SIDFILE_PSID_COPYRIGHT);
+
+	sidType = (char *)(header + SIDFILE_PSID_ID);
+	sidVersion = Read16(header, SIDFILE_PSID_VERSION);
 
 	// Seek to start of module data
 	fseek(f, Read16(header, SIDFILE_PSID_LENGTH), SEEK_SET);
 
 	// Find load address
 	loadAddr = Read16(header, SIDFILE_PSID_START);
-	if(loadAddr == 0)
+	if (loadAddr == 0)
 	{
 		uint8_t lo = fgetc(f);
 		uint8_t hi = fgetc(f);
 		loadAddr = (hi << 8) | lo;
 	}
-	
-	if(initAddr == 0)
+
+	if (initAddr == 0)
 	{
 		initAddr = loadAddr;
 	}
 
 	// Load module data
 	dataLength = fread(dataBuffer, 1, 0x10000, f);
-	
+
+	// flags start at 0x76
+	sidFlags = Read16(header, SIDFILE_PSID_FLAGS);
+
+	// - Bit 0 specifies format of the binary data (musPlayer):
+	// 0 = built-in music player,
+	// 1 = Compute!'s Sidplayer MUS data, music player must be merged.
+
+	// x = 20; // 0x0014
+	// x = x & 1;
+
+	// - Bit 1 specifies whether the tune is PlaySID specific, e.g. uses PlaySID
+	// samples (psidSpecific):
+	// 0 = C64 compatible,
+	// 1 = PlaySID specific (PSID v2NG)
+	// 1 = C64 BASIC flag (RSID)
+
+	// x = x & 2;
+
+	// - Bits 2-3 specify the video standard (clock):
+	// 00 = Unknown,
+	// 01 = PAL,
+	// 10 = NTSC,
+	// 11 = PAL and NTSC.
+
+	// clockSpeed = (reverse(sidFlags) >> 5) & 3; // 0b00001100 bits 2 & 3 ~ but from reverse bits
+	clockSpeed = (sidFlags >> 2) & 3; // 0b00001100 bits 2 & 3 ~ but from reverse bits
+
+	// - Bits 4-5 specify the SID version (sidModel):
+	// 00 = Unknown,
+	// 01 = MOS6581,
+	// 10 = MOS8580,
+	// 11 = MOS6581 and MOS8580.
+
+	// chipType = (reverse(sidFlags) >> 3) & 3; // 0b00110000 bits 4 & 5 ~ but from reverse bits
+	chipType = (sidFlags >> 4) & 3; // 0b00110000 bits 4 & 5 ~ but from reverse bits
+
 	fclose(f);
 
 	return SIDFILE_OK;
+}
+
+string SidFile::GetSidType()
+{
+	return sidType;
 }
 
 string SidFile::GetModuleName()
@@ -134,6 +186,31 @@ uint16_t SidFile::GetDataLength()
 	return dataLength;
 }
 
+uint16_t SidFile::GetSidVersion()
+{
+	return sidVersion;
+}
+
+uint16_t SidFile::GetSidFlags()
+{
+	return sidFlags;
+}
+
+uint16_t SidFile::GetChipType()
+{
+	return chipType;
+}
+
+uint16_t SidFile::GetClockSpeed()
+{
+	return clockSpeed;
+}
+
+uint16_t SidFile::GetDataOffset()
+{
+	return dataOffset;
+}
+
 uint16_t SidFile::GetLoadAddress()
 {
 	return loadAddr;
@@ -148,4 +225,3 @@ uint16_t SidFile::GetPlayAddress()
 {
 	return playAddr;
 }
-
--- src/SidFile.h
+++ src/SidFile.h
@@ -9,63 +9,71 @@
 #include <string.h>
 using namespace std;
 
-#define PSID_MIN_HEADER_LENGTH 118		// Version 1
-#define PSID_MAX_HEADER_LENGTH 124		// Version 2
+#define PSID_MIN_HEADER_LENGTH 118 // Version 1
+#define PSID_MAX_HEADER_LENGTH 124 // Version 2
 
 // Offsets of fields in header (all fields big-endian)
-enum {
-	SIDFILE_PSID_ID = 0,			// 'PSID'
-	SIDFILE_PSID_VERSION = 4,		// 1 or 2
-	SIDFILE_PSID_LENGTH = 6,		// Header length
-	SIDFILE_PSID_START = 8,			// C64 load address
-	SIDFILE_PSID_INIT = 10,			// C64 init routine address
-	SIDFILE_PSID_MAIN = 12,			// C64 replay routine address
-	SIDFILE_PSID_NUMBER = 14,		// Number of subsongs
-	SIDFILE_PSID_DEFSONG = 16,		// Main subsong number
-	SIDFILE_PSID_SPEED = 18,		// Speed flags (1 bit/song)
-	SIDFILE_PSID_NAME = 22,			// Module name (ISO Latin1 character set)
-	SIDFILE_PSID_AUTHOR = 54,		// Author name (dto.)
-	SIDFILE_PSID_COPYRIGHT = 86,	// Copyright info (dto.)
-	SIDFILE_PSID_FLAGS = 118,		// Flags (only in version 2 header)
+enum
+{
+	SIDFILE_PSID_ID = 0,		 // 'PSID'
+	SIDFILE_PSID_VERSION = 4,	 // 1 or 2
+	SIDFILE_PSID_LENGTH = 6,	 // Header length
+	SIDFILE_PSID_START = 8,		 // C64 load address
+	SIDFILE_PSID_INIT = 10,		 // C64 init routine address
+	SIDFILE_PSID_MAIN = 12,		 // C64 replay routine address
+	SIDFILE_PSID_NUMBER = 14,	 // Number of subsongs
+	SIDFILE_PSID_DEFSONG = 16,	 // Main subsong number
+	SIDFILE_PSID_SPEED = 18,	 // Speed flags (1 bit/song)
+	SIDFILE_PSID_NAME = 22,		 // Module name (ISO Latin1 character set)
+	SIDFILE_PSID_AUTHOR = 54,	 // Author name (dto.)
+	SIDFILE_PSID_COPYRIGHT = 86, // Release year and Copyright info (dto.)
+	SIDFILE_PSID_FLAGS = 118,	 // Flags (only in version 2 header)
 	SIDFILE_PSID_RESERVED = 120
 };
 
-enum {
+enum
+{
 	SIDFILE_OK,
 	SIDFILE_ERROR_FILENOTFOUND,
 	SIDFILE_ERROR_MALFORMED
 };
 
-enum {
+enum
+{
 	SIDFILE_SPEED_50HZ,
 	SIDFILE_SPEED_60HZ
 };
 
-
 class SidFile
 {
 private:
-	
 	string moduleName;
 	string authorName;
 	string copyrightInfo;
+	string sidType;
 	int numOfSongs;
 	int firstSong;
+	uint16_t sidVersion;
+	uint16_t dataOffset;
 	uint16_t initAddr;
 	uint16_t playAddr;
 	uint64_t loadAddr;
 	uint32_t speedFlags;
 	uint8_t dataBuffer[0x10000];
 	uint16_t dataLength;
-	
+	uint16_t sidFlags;
+	uint16_t clockSpeed;
+	uint16_t chipType;
+
 	uint16_t Read16(const uint8_t *p, int offset);
 	uint32_t Read32(const uint8_t *p, int offset);
 	bool IsPSIDHeader(const uint8_t *p);
-	
+	unsigned char reverse(unsigned char b);
+
 public:
-	
 	SidFile();
 	int Parse(string file);
+	string GetSidType();
 	string GetModuleName();
 	string GetAuthorName();
 	string GetCopyrightInfo();
@@ -74,6 +82,11 @@ public:
 	int GetFirstSong();
 	uint8_t *GetDataPtr();
 	uint16_t GetDataLength();
+	uint16_t GetSidVersion();
+	uint16_t GetSidFlags();
+	uint16_t GetChipType();
+	uint16_t GetClockSpeed();
+	uint16_t GetDataOffset();
 	uint16_t GetLoadAddress();
 	uint16_t GetInitAddress();
 	uint16_t GetPlayAddress();
