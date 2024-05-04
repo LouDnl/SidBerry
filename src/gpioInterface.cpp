b53fc092ab34bf39ff06d2e8d2aaac02f5c7c377
loudnl <LouDnl@users.noreply.github.com>
2024-05-04 12:31:03 +0200

    add ftdi ft2232 board add gpioStop (ftdi only) add gpioRead (ftdi only) fix no return error

--- src/gpioInterface.cpp
+++ src/gpioInterface.cpp
@@ -5,60 +5,136 @@
 
 #include "gpioInterface.h"
 
-#if BOARD == RASPBERRYPI				//Raspberry Pi (GPIO library: <wiringPi.h>)
+#if BOARD == RASPBERRYPI // Raspberry Pi (GPIO library: <wiringPi.h>)
 
-void gpioWrite(int pin, int level){
+int gpioSetup()
+{
+	wiringPiSetup();
+
+	return 0;
+}
+
+int gpioStop()
+{
+	return 0;
+}
+
+void gpioMode(int pin, int mode)
+{
+	pinMode(pin, mode);
+}
+
+int gpioRead(int pin)
+{
+	return 0;
+}
+
+void gpioWrite(int pin, int level)
+{
 	digitalWrite(pin, level);
 }
 
-int gpioSetup(){
-	wiringPiSetup();
+#elif BOARD == ARIETTAG25 // Acme Systems Arietta G25 (GPIO library: <wiringSam.h>)
+
+int gpioSetup()
+{
+	wiringSamSetup();
+	return 0;
 }
 
-void gpioMode(int pin, int mode){
+int gpioStop()
+{
+	return 0;
+}
+
+void gpioMode(int pin, int mode)
+{
+	FILE *fp = fopen("/sys/class/gpio/export", "w");
+	fprintf(fp, "%d", pin);
+	fclose(fp);
+	if (pin >= 0 && pin < 32)
+		pinMode(WSAM_PIO_A, pin, mode);
+	else if (pin >= 32 && pin < 64)
+		pinMode(WSAM_PIO_B, pin - 32, mode);
+	else if (pin >= 64 && pin < 96)
+		pinMode(WSAM_PIO_C, pin - 64, mode);
+}
+
+int gpioRead(int pin)
+{
+	return 0;
+}
+
+void gpioWrite(int pin, int level)
+{
+	if (pin >= 0 && pin < 32)
+		digitalWrite(WSAM_PIO_A, pin, level);
+	else if (pin >= 32 && pin < 64)
+		digitalWrite(WSAM_PIO_B, pin - 32, level);
+	else if (pin >= 64 && pin < 96)
+		digitalWrite(WSAM_PIO_C, pin - 64, level);
+}
+
+#elif BOARD == FTDI // FTDI ft2232h ~ requires libftdi libftdi-dev libftdi1 libftdi1-dev
+
+int gpioSetup()
+{
+	initPort(PORT1);
+	enableBitBang(PORT1);
+	initPort(PORT2);
+	enableBitBang(PORT2);
+
+	return 0;
+}
+
+int gpioStop()
+{
+	disableBitBang(PORT1);
+	closePort(PORT1);
+	disableBitBang(PORT2);
+	closePort(PORT2);
+
+	return 0;
+}
+
+int gpioRead(int pin)
+{
+	return digitalRead(pin);
+}
+
+void gpioWrite(int pin, int level)
+{
+	digitalWrite(pin, level);
+}
+
+void gpioMode(int pin, int mode)
+{
 	pinMode(pin, mode);
 }
-	
-#elif BOARD == ARIETTAG25				//Acme Systems Arietta G25 (GPIO library: <wiringSam.h>)
 
-void gpioWrite(int pin, int level){
-	if (pin>=0 && pin<32) digitalWrite(WSAM_PIO_A, pin, level);
-	else if (pin>=32 && pin<64) digitalWrite(WSAM_PIO_B, pin-32, level);
-	else if (pin>=64 && pin<96) digitalWrite(WSAM_PIO_C, pin-64, level);
+#elif BOARD == CUSTOM // Custom Board
+
+int gpioSetup()
+{
+	return 0;
 }
 
-int gpioSetup(){
-	wiringSamSetup();
+int gpioStop()
+{
+	return 0;
 }
 
-void gpioMode(int pin, int mode){
-	FILE *fp=fopen("/sys/class/gpio/export","w");
-	fprintf(fp,"%d",pin);
-	fclose(fp);
-	if (pin>=0 && pin<32) pinMode(WSAM_PIO_A, pin, mode);
-	else if (pin>=32 && pin<64) pinMode(WSAM_PIO_B, pin-32, mode);
-	else if (pin>=64 && pin<96) pinMode(WSAM_PIO_C, pin-64, mode);
+void gpioMode(int pin, int mode)
+{
 }
 
-#elif BOARD == CUSTOM					//Custom Board
-
-/*
- *	Function to WRITE to a GPIO port. Map this function to the equivalent GPIO library function or write it.
- */
-void gpioWrite(int pin, int level){
-	
+int gpioRead(int pin)
+{
+	return 0;
 }
 
-/*
- *	Function to SETUP the GPIO ports. Map this function to the equivalent GPIO library function or write it.
- */
-int gpioSetup(){
+void gpioWrite(int pin, int level)
+{
 }
 
-/*
- *	Function to set the MODE of a GPIO port. Map this function to the equivalent GPIO library function or write it.
- */
-void gpioMode(int pin, int mode){
-}
-	
-#endif
+#endif // BOARD
--- src/gpioInterface.h
+++ src/gpioInterface.h
@@ -1,60 +1,144 @@
+#pragma once
+#ifndef GPIOINTERFACE_H
+#define GPIOINTERFACE_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
-#define RASPBERRYPI	0
-#define ARIETTAG25	1
-#define CUSTOM		2
+#define FTDI 0
+#define RASPBERRYPI 1
+#define ARIETTAG25 2
+#define CUSTOM 3
+
+// #define BOARD FTDI  // enable to always compile FTDI
+
 //============================================================================
-// Description : SidBerry wrapper to GPIO ports - Header File
-// Author      : Alessio Lombardo
+// Description  : SidBerry wrapper to GPIO ports - Header File
+// Author       : Alessio Lombardo
+// Modifications: LouD
 //============================================================================
 
-#if BOARD == RASPBERRYPI				//Raspberry Pi
+#if BOARD == RASPBERRYPI // Raspberry Pi
+extern "C"
+{
+#include <wiringPi.h>
+}
 
-	#include <wiringPi.h>
-	
-	#define CS		3
-	#define A0		8
-	#define A1		9
-	#define A2		7
-	#define A3		0
-	#define A4		2
-	#define D0		15
-	#define D1		16
-	#define D2		1
-	#define D3		4
-	#define D4		5
-	#define D5		6
-	#define D6		10
-	#define D7		11
-	
-#elif BOARD == ARIETTAG25				//Acme Systems Arietta G25
+/* WiringPi pin numbers */
+// #define CLK            21
+#define RES 25 // BCM26
+#define RW 24  // BCM19
+#define CS 3   // BCM22
 
-	#include <wiringSam.h>
-	
-	#define CS		65
-	#define A0		6
-	#define A1		92
-	#define A2		68
-	#define A3		67
-	#define A4		66
-	#define D0		8
-	#define D1		0
-	#define D2		29
-	#define D3		28
-	#define D4		27
-	#define D5		26
-	#define D6		25
-	#define D7		24
+#define A0 8 // BCM02
+#define A1 9 // BCM03
+#define A2 7 // BCM04
+#define A3 0 // BCM17
+#define A4 2 // BCM27
 
-#elif BOARD == CUSTOM					//Custom Board
-	
-	//1) GPIO library (if needed)
-	//2) I/O Defines (if needed): INPUT mode, OUTPUT mode, HIGH level, LOW level
-	//3) Pinout Defines (mandatory): CS (Chip Select), A0-A4 (Address Bus), D0-D7 (Data Bus)
-	
-#endif
+#define D7 1  // BCM18
+#define D6 4  // BCM23
+#define D5 5  // BCM24
+#define D4 6  // BCM25
+#define D3 26 // BCM12
+#define D2 27 // BCM16
+#define D1 28 // BCM20
+#define D0 29 // BCM21
 
-void gpioWrite(int pin, int level);
+#elif BOARD == ARIETTAG25 // Acme Systems Arietta G25
+
+#include <wiringSam.h>
+
+#define CS 65
+#define A0 6
+#define A1 92
+#define A2 68
+#define A3 67
+#define A4 66
+#define D0 8
+#define D1 0
+#define D2 29
+#define D3 28
+#define D4 27
+#define D5 26
+#define D6 25
+#define D7 24
+
+#elif BOARD == FTDI // FTDI ft2232h
+
+#include "gpio.h"
+
+#define RES BD0
+#define RW BD1
+#define CS BD2
+
+#define A0 BD3
+#define A1 BD4
+#define A2 BD5
+#define A3 BD6
+#define A4 BD7
+
+#define D0 AD0
+#define D1 AD1
+#define D2 AD2
+#define D3 AD3
+#define D4 AD4
+#define D5 AD5
+#define D6 AD6
+#define D7 AD7
+
+#elif BOARD == CUSTOM // Custom Board
+
+// 1) GPIO library (if needed)
+// 2) I/O Defines (if needed): INPUT mode, OUTPUT mode, HIGH level, LOW level
+// 3) Pinout Defines (mandatory): CS (Chip Select), A0-A4 (Address Bus), D0-D7 (Data Bus)
+
+// REQUIRED
+#define HIGH 1
+#define LOW 0
+#define OUTPUT 1
+#define INPUT 0
+
+// #define RES 0
+// #define RW 0
+#define CS 0
+
+#define A0 0
+#define A1 0
+#define A2 0
+#define A3 0
+#define A4 0
+
+#define D0 0
+#define D1 0
+#define D2 0
+#define D3 0
+#define D4 0
+#define D5 0
+#define D6 0
+#define D7 0
+
+#endif // BOARD
+
+/*
+ *	Function to SETUP the GPIO ports. Map this function to the equivalent GPIO library function or write it.
+ */
 int gpioSetup();
+/*
+ *	Function to break down the GPIO ports.
+ */
+int gpioStop();
+/*
+ *	Function to set the MODE of a GPIO port. Map this function to the equivalent GPIO library function or write it.
+ */
 void gpioMode(int pin, int mode);
+/*
+ *	Function to READ a pin state from a GPIO port.
+ */
+int gpioRead(int pin);
+/*
+ *	Function to WRITE to a GPIO port. Map this function to the equivalent GPIO library function or write it.
+ */
+void gpioWrite(int pin, int level);
+
+#endif // GPIOINTERFACE_H
