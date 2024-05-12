80c0dd43cda6d8b0e456117b2f4f10503e0d9491
loudnl <LouDnl@users.noreply.github.com>
2024-05-12 12:37:36 +0200

    Add clock and hertz defines and vars - move some vars from src to header

--- src/sidberry.h
+++ src/sidberry.h
@@ -31,30 +31,33 @@ using namespace std;
 #define CLOCK_CYCLES 100000
 
 // Clock speed: 0.985 MHz (PAL) or 1.023 MHz (NTSC)
-#define DEFAULT_CLOCK 1000000 // 1MHz = 1us
-#define PAL_CLOCK 985000      // 0.985 MHz
-#define NTSC_CLOCK 1023000    // 1.023 MHz
+#define CLOCK_DEFAULT 1000000  // 1MHz = 1us
+#define CLOCK_PAL      985248  // 0.985 MHz
+#define CLOCK_NTSC    1022727  // 1.023 MHz
+#define CLOCK_DREAN   1023440  // 1.023 MHz
 
 // Refreshrates in microseconds
-#define HERTZ_50 20000 // 50Hz ~ 20us
-#define HERTZ_60 16667 // 60Hz ~ 16.67us
+#define HERTZ_DEFAULT 20000  // 50Hz ~ 20000 == 20us
+#define HERTZ_50      19950  // 50Hz ~ 20000 == 20us / 50.125Hz ~ 19.950124688279us exact
+#define HERTZ_60      16715  // 60Hz ~ 16667 == 16.67us / 59.826Hz ~ 16.715140574332 exact
 
 enum clock_speeds
 {
-    UNKNOWN = PAL_CLOCK,
-    PAL = PAL_CLOCK,
-    NTSC = NTSC_CLOCK,
-    BOTH = DEFAULT_CLOCK
+    UNKNOWN = CLOCK_DEFAULT,
+    PAL     = CLOCK_PAL,
+    NTSC    = CLOCK_NTSC,
+    BOTH    = CLOCK_NTSC,
+    DREAN   = CLOCK_DREAN,
 
 };
-static const enum clock_speeds clockSpeed[] = {UNKNOWN, PAL, NTSC, BOTH};
+static const enum clock_speeds clockSpeed[] = {UNKNOWN, PAL, NTSC, BOTH, DREAN};
 
 enum refresh_rates
 {
-    DEFAULT = HERTZ_50,
-    EU = HERTZ_50,
-    US = HERTZ_60,
-    GLOBAL = HERTZ_60
+    DEFAULT = HERTZ_DEFAULT,
+    EU      = HERTZ_50,
+    US      = HERTZ_60,
+    GLOBAL  = HERTZ_60
 };
 static const enum refresh_rates refreshRate[] = {DEFAULT, EU, US, GLOBAL};
 
@@ -62,6 +65,11 @@ static const enum refresh_rates refreshRate[] = {DEFAULT, EU, US, GLOBAL};
     printf("enum index %i\n", (clock_speed)cs); // index
     printf("enum value %i\n", clockSpeed[cs]);	// value
 */
+extern timeval t1, t2;
+extern long int elaps;
+extern int custom_clock;
+extern int custom_hertz;
+extern int volume;
 extern int clock_speed;
 extern int refresh_rate;
 extern uint8_t memory[65536];
@@ -70,6 +78,7 @@ extern bool verbose;
 extern bool trace;
 extern bool exclock;
 extern bool exrefresh;
+extern bool real_read;
 extern volatile sig_atomic_t stop;
 
 void inthand(int signum);
